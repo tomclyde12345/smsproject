766897721b3705c8e69af7991b69df9ec1e051af
tomclyde12345 <47425217+tomclyde12345@users.noreply.github.com>
2022-04-19 09:56:48 +0800

    latest

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -43,18 +43,57 @@ namespace smsproject.Controllers
             };
             return View("Create", vm);
         }
+        public ActionResult Edit(int id)
+        {
+            var user = _context.Users.SingleOrDefault(c => c.Id == id);
+            if (user == null)
+                return HttpNotFound();
+            return View("Edit", user);
+        }
 
         public ActionResult Save(Users users)
+
         {
             if (users.Id == 0)
             {
                 users.DateAdded = DateTime.Now;
                 _context.Users.Add(users);
+
                 
             }
+            else
+            {
+                var usersInDb = _context.Users.Single(c => c.Id == users.Id);
 
+                usersInDb.Name = users.Name;
+                usersInDb.Address = users.Address;
+                usersInDb.DateAdded = users.DateAdded;
+                usersInDb.EmailAddress = users.EmailAddress;
+                usersInDb.age = users.age;
+            }
             _context.SaveChanges();
             return RedirectToAction("Index", "Users");
+
         }
+        //  [HttpPost]
+        // // public ActionResult Save(Users sUsers)
+
+        //      if (sUsers.Id == 0)
+        //      _context.Users.Add(users);
+        //  else
+        //  {
+        //  var usersInDb = _context.Users.Single(c => c.Id == users.Id);
+        //
+        //    usersInDb.Name = users.Name;
+        //   usersInDb.Address = users.Address;
+        //   usersInDb.DateAdded = users.DateAdded;
+        //   usersInDb.EmailAddress = users.EmailAddress;
+        //  usersInDb.age = users.age;
+
+
+
+
     }
+
 }
+
