7c3a4189d6ac96aeb43921e0ff5b87086ed299ed
tomclyde12345 <47425217+tomclyde12345@users.noreply.github.com>
2022-04-21 15:40:50 +0800

    - working edit and create

--- Models/Users.cs
+++ Models/Users.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,9 @@ namespace smsproject.Models
     public class Users
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public string Address { get; set; }
         public DateTime? DateAdded { get; set; }
